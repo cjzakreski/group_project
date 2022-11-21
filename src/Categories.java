c3ded5535b638f3bd7e9864ba9d19ad7ba907499
Abigail <abigail.qianhui@gmail.com>
2022-11-21 17:52:54 -0500

    updated Categories class

--- src/Categories.java
+++ src/Categories.java
@@ -1,2 +1,16 @@
+import java.io.FileReader;
+import java.util.ArrayList;
+
 public class Categories {
+    private String filename;
+    private FileReader reader;
+    private ArrayList<String> wordlist;
+
+    public Categories(String filename) {
+        this.filename = filename;
+    }
+
+    public String getWord() {
+        return "";
+    }
 }
