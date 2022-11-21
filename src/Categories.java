cd8d94ea589101d585d7e12bf8f1d004b85c1381
Abigail <abigail.qianhui@gmail.com>
2022-11-21 18:28:29 -0500

    finished Categories

--- src/Categories.java
+++ src/Categories.java
@@ -1,16 +1,38 @@
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Categories {
     private String filename;
     private FileReader reader;
-    private ArrayList<String> wordlist;
+    private ArrayList<String> wordList;
 
     public Categories(String filename) {
         this.filename = filename;
+
+        Scanner in = null;
+        try {
+            reader = new FileReader(filename);
+            in = new Scanner(reader);
+        } catch (FileNotFoundException e) {
+            System.out.println("Error, file could not be found!");
+            System.exit(-1);
+        }
+
+        while(in.hasNext()) {
+            String word = in.nextLine();
+            wordList.add(word);
+        }
     }
 
     public String getWord() {
-        return "";
+        Random rand = new Random();
+        int upper = wordList.size();
+        int index = rand.nextInt(upper);
+        String targetWord = wordList.get(index);
+
+        return targetWord;
     }
 }
