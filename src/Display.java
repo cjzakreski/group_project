6c1e0b4e57aa548c2d16e9aa4f39030ca16e0ef3
cjzakreski <117182607+cjzakreski@users.noreply.github.com>
2022-11-21 17:50:33 -0500

    start menu display added

--- src/Display.java
+++ src/Display.java
@@ -1,5 +1,18 @@
 import javax.swing.*;
 import java.awt.*;
 public class Display {
+    public Display() {
+        JFrame startMenu = new JFrame("Start Menu");
+        startMenu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        startMenu.getContentPane().setLayout(new BoxLayout(startMenu.getContentPane(), BoxLayout.Y_AXIS));
 
+        JLabel categories = new JLabel("Select a category");
+        JButton animals = new JButton("Animals");
+
+        startMenu.getContentPane().add(categories);
+        startMenu.getContentPane().add(animals);
+
+        startMenu.pack();
+        startMenu.setVisible(true);
+    }
 }
