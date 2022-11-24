27bb0511ec4dacade7afb38b99af07c427b51c2f
selbyheyman <selbyheyman@yahoo.com>
2022-11-23 23:39:52 -0500

    updated highscores class

--- src/HighScores.java
+++ src/HighScores.java
@@ -1,6 +1,4 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -16,73 +14,61 @@ public class HighScores {
     }
 
     public HighScores(Scanner sc) throws IOException {
-        String path = System.getProperty("user.home") + File.separator + "HangmanScores";
-        try {
-            File scoresFile = new File(path);
-            if (scoresFile.exists()) {
-                sc = new Scanner(scoresFile);
-                while (sc.hasNextLine()) {
-                    this.name = sc.nextLine();
-                    this.numGuesses = Integer.parseInt(sc.nextLine());
-                    this.category = sc.nextLine();
-                }
-            } else {
-                boolean success = scoresFile.createNewFile();
-                if (!success) {
-
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-        // checks to see if directory was successfully created
-        // needs to catch the exception that the directory could not be created
-        boolean dirCreated = new File(path).mkdirs();
-        if (dirCreated) {
-
-        }
-
-        // gets the highscores file if it exists, if not,
-        // creates a new scores file in the user's hangman directory
-        File scoresFile = new File(path + "/HighScores.txt");
-        if (!scoresFile.exists()) {
-            boolean success = scoresFile.createNewFile();
-            if (!success) {
-                // catch
-            }
+        while (sc.hasNextLine()) {
+            this.name = sc.nextLine();
+            this.numGuesses = Integer.parseInt(sc.nextLine());
+            this.category = sc.nextLine();
         }
     }
 
     // compares the score the scores from the file if it exists, if not, writes it into the file
-    public void compareScores() throws FileNotFoundException {
-        String path = System.getProperty("user.home") + File.separator + "HangmanScores";
+    public void compareScores() throws IOException {
+        HighScores newScore = new HighScores(this.name, this.numGuesses, this.category);
+        String path = System.getProperty("user.home") + File.separator + "HighScores.txt";
         File scoresFile = new File(path);
+        if (!scoresFile.exists()) {
+            boolean success = scoresFile.createNewFile();
+            if (!success) {
+                // catch exception that file could not be created
+            }
+        }
+
         ArrayList<HighScores> hsList= new ArrayList<>();
         try {
-            if (scoresFile.exists()) {
-                Scanner sc = new Scanner(scoresFile);
-                while (sc.hasNextLine()) {
-                    HighScores hs = new HighScores(sc.nextLine(), Integer.parseInt(sc.nextLine()), sc.nextLine());
-                    hsList.add(hs);
-                }
-            } else {
-                boolean success = scoresFile.createNewFile();
-                if (!success) {
-                    // catch exception that file could not be created
-                }
+            Scanner sc = new Scanner(scoresFile);
+            while (sc.hasNextLine()) {
+                HighScores hs = new HighScores(sc);
+                hsList.add(hs);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+        // loops through to compare the highscore to the highscores in the list
+        // inserting it at the correct ranking
         for (HighScores hs : hsList) {
-            if (this.category.equals(hs.getCategory())); {
-                if (this.numGuesses>hs.getNumGuesses());
-                // save all high scores from the list, making a new list, and then
-                // overwrite the new high scores into the file
+            if (this.numGuesses > hs.getNumGuesses()) {
+                hsList.add(hsList.indexOf(hs), newScore);
+                continue;
             }
+        }
+        if (hsList.size()==5) {
+            hsList.remove(4);
+        }
+
+        // overwrite the scores of hsList into the highscores file
+        PrintWriter writer = null;
+        try {
+            writer = new PrintWriter(new FileOutputStream(path, false));
+            for (HighScores hs : hsList) {
+                writer.println(hs.getName());
+                writer.println(hs.getNumGuesses());
+                writer.println(hs.getCategory());
+            }
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -96,17 +82,5 @@ public class HighScores {
 
     public String getCategory() {
         return this.category;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setNumGuesses(int num) {
-        this.numGuesses = num;
-    }
-
-    public void setCategory() {
-        this.category = category;
     }
 }
